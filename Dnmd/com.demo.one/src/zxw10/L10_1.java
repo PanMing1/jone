f783aebc3961fa386d2c45ce2e696de9cd47ca0d
PanMing1 <1369431412@qq.com>
2021-02-04 18:21:24 +0800

    1

---
+++ Dnmd/com.demo.one/src/zxw10/L10_1.java
@@ -0,0 +1,39 @@
+package zxw10;
+
+import java.sql.*;
+
+public class L10_1 {
+	public static void main(String[] args) {
+		Connection ct = null;
+		Statement sm = null;
+		try {
+			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+			ct = DriverManager.getConnection("jdbc:odbc:sql server", "sa", "root");
+			sm = ct.createStatement();
+
+			ResultSet rs = sm.executeQuery("select * from renwu");
+
+			while (rs.next()) {
+				int bianhao = rs.getInt(1);
+				String xiangming = rs.getString(2);
+				String zhiwu = rs.getString(3);
+				System.out.println(bianhao + "------" + xiangming + "------" + zhiwu);
+			}
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		} finally {
+			try {
+				if (sm != null) {
+					sm.close();
+				}
+				if (ct != null) {
+					ct.close();
+				}
+			} catch (Exception e2) {
+
+			}
+
+		}
+	}
+}
