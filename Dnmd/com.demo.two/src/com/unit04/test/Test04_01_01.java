0c923a97d9c6a5c95c547151144ca6dcade13d55
PanMing1 <1369431412@qq.com>
2021-05-27 18:01:14 +0800

    20210527011

--- Dnmd/com.demo.two/src/com/unit04/test/Test04_01_01.java
+++ Dnmd/com.demo.two/src/com/unit04/test/Test04_01_01.java
@@ -1,5 +1,38 @@
 package com.unit04.test;
 
-public class Test04_01_01 {
+import java.util.Random;
 
+public class Test04_01_01 {
+	public static void main(String[] args) {
+		Random r = new Random();
+		int a;
+		int b[] = new int[6];
+		b[0] = r.nextInt(33) + 1;
+		int j = 1;
+		while (j <= b.length - 1) {
+			int c = r.nextInt(33) + 1;
+			boolean d = true;
+			for (int i = 0; i < j; i++) {
+				if (c == b[i]) {
+					d = false;
+					break;
+				}
+			}
+			if (d) {
+				b[j] = c;
+				for (int i = j; i > 0; i--) {
+					if (b[i] < b[i - 1]) {
+						int e = b[i];
+						b[i] = b[i - 1];
+						b[i - 1] = e;
+					}
+				}
+				j++;
+			}
+		}
+		for (int i : b) {
+			System.out.print(i);
+			System.out.print("\t");
+		}
+	}
 }
---
+++ Dnmd/com.demo.two/src/com/unit04/test/Test04_01_02.java
@@ -0,0 +1,13 @@
+package com.unit04.test;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Test04_01_02 {
+	public static void main(String[] args) {
+		int a[] = new int[2];
+		a[0] = 1;
+		a[1] = 2;
+		System.out.println(a);
+	}
+}
